1aa04b9dadbdb91fd42a5f793071183c51051813
Frédéric Loulergue <frederic.loulergue@univ-orleans.fr>
2024-09-29 18:01:56 +0200

    Modification du générateur : messages affichés lors de la génération

--- scripts/generateur.sh
+++ scripts/generateur.sh
@@ -4,10 +4,13 @@ OUTPUT=www/livres
 
 if [ ! -d $OUTPUT ]
 then
+    echo "Création d'un répertoire pour les pages HTML de livres."
     mkdir $OUTPUT
 fi
 
 tail -n +2 data/livres.csv > data/tmp.csv
+
+echo -n "Génération des pages des livres" 
 
 IFS="|"
 COUNT=0
@@ -20,12 +23,23 @@ while read Auteur Titre Editeur Annee ISBN QCouv; do
     sed s/%ANNEE%/"$Annee"/ |
     sed s/%ISBN%/"$File"/ |
     sed s/%QCOUV%/"$QCouv"/ > $OUTPUT/$File.html
+    echo -n "."
     COUNT=$(expr $COUNT + 1)
 done < data/tmp.csv
 
+echo 
+if [ "$COUNT" = "1" ] || [ "$COUNT" = "0" ]
+then
+    echo "$COUNT page générée."
+else 
+    echo "$COUNT pages générées."
+fi
+
+echo Génération du tableau des livres.
 cat templates/livres_debut.html > www/livres.html
 while read Auteur Titre Editeur Annee ISBN QCouv; do 
     File=${ISBN//-/}; File=${File// /}; File=${File// /}
     echo "<tr><td>$Auteur</td><td><a href=\"livres/$File.html\">$Titre</a></td><td>$File</td><tr>" >> www/livres.html
 done < data/tmp.csv
 cat templates/livres_fin.html >> www/livres.html
+
